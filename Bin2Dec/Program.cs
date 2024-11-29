4e7c8f801bba92d19ee5ec6a3e3c16fc34653988
Geremy Brooks <brooksgeremy@outlook.com>
2024-11-29 15:49:51 -0600

    Binary to decimal number converter. i need a better understanding of the convertions but i understand the structer of the code.

--- Bin2Dec/Program.cs
+++ Bin2Dec/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string binaryString = "101";
+            decimal decimalValue = BinaryToDecimal(binaryString);
+            Console.WriteLine($"The decimal value of binary {binaryString} is {decimalValue}");
+        }
+
+        static decimal BinaryToDecimal(string binary)
+        {
+            decimal decimalValue = 0;
+            decimal baseValue = 1;
+
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                if (binary[i] == '1')
+                {
+                    decimalValue += baseValue;
+                }
+                baseValue *= 2;
+            }
+            return decimalValue;
         }
     }
 }
